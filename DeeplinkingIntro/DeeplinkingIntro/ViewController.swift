dc3a2aaa04cc4ae1d46f3c9e8efee18c9e839b12
ZinMinPhyo <zinminphyo@better.hr>
2021-06-06 08:45:35 +0630

    Project Setup.

---
+++ DeeplinkingIntro/DeeplinkingIntro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeeplinkingIntro
+//
+//  Created by zin min phyo on 06/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
